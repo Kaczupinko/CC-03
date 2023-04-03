31abc734c58000e020278d3ca4c27e9319aaec3f
Kaczupinko <119810971+Kaczupinko@users.noreply.github.com>
2023-04-03 23:06:59 +0100

    Update Exercise.js

--- Exercise.js
+++ Exercise.js
@@ -247,3 +247,43 @@ function wyswietlTemperatury(temperatura, jednostka) {
 wyswietlTemperatury(100, 'F');
 wyswietlTemperatury(38, 'C');
 */
+const emojiMappings = {
+    'stop': '🚫',
+    'gwiazda': '⭐️',
+    'samochód': '🏎',
+    'buduję': '🧱',
+    'budzik': '⏰'
+};
+
+function useEmoji(input) {
+    let result = input;
+    for (let key in emojiMappings) {
+        result = result.replace(key, emojiMappings[key]);
+    }
+    return result;
+
+
+    function verify(input, goal) {
+        if (input === goal) {
+            console.log('Gratulacje!');
+        } else {
+            console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+        }
+    }
+
+    verify(
+        useEmoji("Takiemu zachowaniu mówię stop i to mocny estop!"),
+        "Takiemu zachowaniu mówię 🚫 i to mocny estop!"
+    );
+    verify(useEmoji("Jadę po nowy samochód :D"), "Jadę po nowy 🏎 :D");
+    verify(useEmoji("Jadę po nowy samochódy :D"), "Jadę po nowy samochódy :D");
+    verify(
+        useEmoji("Właśnie buduję swoje skille w JS"),
+        "Właśnie 🧱 swoje skille w JS"
+    );
+    verify(
+        useEmoji("Właśnie nadbuduję swoje skille w JS"),
+        "Właśnie nadbuduję swoje skille w JS"
+    );
+    verify(useEmoji("Buduję samochód"), "🧱 🏎");
+    verify(useEmoji("BuDuję SaMocHód."), "🧱 🏎.");
